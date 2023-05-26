40634895ae9c98a9a997d1eba7d0ee99116c7b52
Juan José Vázquez Gil <juanjose.vazquez@prefapp.es>
2023-05-26 12:47:29 +0200

    Removed unused parameter from task_scheduler function

--- firestarter/workflows/ci/task_manager.py
+++ firestarter/workflows/ci/task_manager.py
@@ -2,9 +2,7 @@ from firestarter.common.validations import validate_task_manager
 from .tasks import TaskGroup, Task
 
 
-def task_scheduler(
-    task_manager_data: str, tasks_instance: TaskGroup, context: dict
-) -> None:
+def task_scheduler(task_manager_data: str, tasks_instance: TaskGroup) -> None:
     for index, task in enumerate(task_manager_data["tasks"]):
         task_obj: Task = Task(task, index)
         tasks_instance.add_task(task_obj)
@@ -14,25 +12,25 @@ class TaskManager:
 
     def __init__(self) -> None:
         self.tasks: TaskGroup = TaskGroup()
-        self._context: dict = False
+        self._context = False
 
     @property
     def context(self) -> dict:
         return self._context
 
     @context.setter
-    def context(self, context: dict) -> None:
+    def context(self, context) -> None:
         self._context = context
 
     def load(self, path: str, schema_path: str) -> None:
         task_manager_data: dict = validate_task_manager(path, schema_path)
 
         # init the context properly
-        self.context.default_image: str = task_manager_data["image"]
-        self.context.default_env: dict = task_manager_data.get("vars", {})
+        self.context.default_image = task_manager_data["image"]
+        self.context.default_env = task_manager_data.get("vars", {})
 
         # let's run the tasks
-        task_scheduler(task_manager_data, self.tasks, self.context)
+        task_scheduler(task_manager_data, self.tasks)
 
     def run(self) -> None:
         self.tasks.run_tasks(self.context)
