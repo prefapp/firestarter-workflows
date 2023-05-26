3dffc63f6600f18347ce2070e1387682837fed32
Juan José Vázquez Gil <juanjose.vazquez@prefapp.es>
2023-05-26 09:58:22 +0200

    Removed unnecessary constructor

--- firestarter/workflows/ci/__init__.py
+++ firestarter/workflows/ci/__init__.py
@@ -2,7 +2,7 @@ from .ci import CI
 from firestarter.common.dagger_context import Context
 
 def run(*, vars: dict, secrets: dict, config_file:str) -> None:
-    context: Context = Context(vars=vars)
+    context: Context = Context()
     wf: CI = CI(config_file, context)
     wf.execute()
 
