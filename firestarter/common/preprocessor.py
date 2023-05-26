aeb0ab05c2fb6a9501e6f0178fe12f7357a09b14
Juan José Vázquez Gil <juanjose.vazquez@prefapp.es>
2023-05-26 12:33:07 +0200

    Small preprocessor refactor

--- firestarter/common/preprocessor.py
+++ firestarter/common/preprocessor.py
@@ -21,29 +21,8 @@ class PreProcessor:
     def resolve_symbol(self, symbol: str, context: dict) -> str:
         var_context, var_name = symbol.split(".")
 
-        if var_context == "vars":
-            return self.resolve_var(var_name, context)
-
-        elif var_context == "env":
-            return self.resolve_env(var_name, context)
-
-        elif var_context == "secrets":
-            return self.resolve_secret(var_name, context)
-
-        else:
+        if var_context not in context.keys():
             raise ValueError(f"Unknown context {context} for symbol {symbol}")
 
-
-    def resolve_var(self, var_name: str, context: dict) -> str:
-        return context["resolve_var"](var_name)
-
-    def resolve_env(self, var_name: str, context: dict) -> str:
-        if not var_name in os.environ:
-            raise ValueError(f'{var_name} not found in environment')
-
-        return os.environ[var_name]
-
-
-    def resolve_secret(self, var_name: str, context: dict) -> str:
-        return context["resolve_secret"](var_name)
+        return context[var_context](var_name)
 
--- firestarter/tests/test_preprocessor.py
+++ firestarter/tests/test_preprocessor.py
@@ -7,14 +7,22 @@ def load_yaml(file_path: str) -> str:
     with open(file_path, "r") as f:
         return f.read()
 
+def process_env_var(var_name: str) -> str:
+    if not var_name in os.environ:
+        raise ValueError(f'{var_name} not found in system environment')
+
+    return os.environ[var_name]
+
+
 def process_file(file_path: str) -> str:
     pp: PreProcessor = PreProcessor(load_yaml(os.path.join(
         os.path.dirname(__file__), file_path
     )))
 
     return pp.preprocess({
-        "resolve_var": lambda v: f"VAR_{v}",
-        "resolve_secret": lambda s: f"VAR_{s}",
+        "vars": lambda v: f"VAR_{v}",
+        "secrets": lambda s: f"VAR_{s}",
+        "env": process_env_var,
     })
 
 
@@ -24,7 +32,7 @@ def test_parser() -> None:
     )
 
     assert result.get("a", {}).get("value", "") == "VAR_SECRET1"
-    assert result.get("b", {})[0].get("name", "") == os.environ["SHELL"]
+    assert result.get("b", {})[0].get("name", "") == process_env_var("SHELL")
     assert result.get("b", {})[0].get("value", "") == "VAR_VALUE"
 
 def test_error() -> None:
