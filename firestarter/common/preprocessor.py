9c12661f616d7d65e6bc174fc9ecbacae63d45d2
Juan José Vázquez Gil <juanjose.vazquez@prefapp.es>
2023-05-26 09:32:20 +0200

    Improved tests

--- firestarter/common/preprocessor.py
+++ firestarter/common/preprocessor.py
@@ -1,50 +1,49 @@
 from .config_parser import Parser
 
 import os
-import re
-
-RE_IS_VAR = re.compile("^var\.(\w+)$")
-RE_IS_ENV = re.compile("^env\.(\w+)$")
-RE_IS_SECRET = re.compile("^secret\.(\w+)$")
 
 class PreProcessor:
 
-    def __init__(self, data):
+    def __init__(self, data: str) -> None:
         self.data = data
 
 
-    def preprocess(self, context):
-        parser = Parser(self.data)
+    def preprocess(self, context: dict) -> str:
+        parser: Parser = Parser(self.data)
 
         for symbol in parser.parse():
-            value = self.resolveSymbol(symbol, context)
+            value: str = self.resolveSymbol(symbol, context)
             parser.interpolate(symbol, value)
 
         return parser.data
 
 
-    def resolveSymbol(self, symbol, context):
-        if RE_IS_VAR.match(symbol):
-            return self.resolveVar(symbol, context)
+    def resolveSymbol(self, symbol: str, context: dict) -> str:
+        var_context, var_name = symbol.split(".")
 
-        elif RE_IS_ENV.match(symbol):
-            return self.resolveEnv(symbol, context)
+        if var_context == "vars":
+            return self.resolveVar(var_name, context)
 
-        elif RE_IS_SECRET.match(symbol):
-            return self.resolveSecret(symbol, context)
+        elif var_context == "env":
+            return self.resolveEnv(var_name, context)
+
+        elif var_context == "secrets":
+            return self.resolveSecret(var_name, context)
+
+        else:
+            raise ValueError(f"Unknown context {context} for symbol {symbol}")
 
 
-    def resolveVar(self, symbol, context):
-        return context["resolveVar"](symbol)
+    def resolveVar(self, var_name: str, context: dict) -> str:
+        return context["resolveVar"](var_name)
 
-    def resolveEnv(self, symbol, context):
-        var_name: str = symbol.split(".")[1]
-
+    def resolveEnv(self, var_name: str, context: dict) -> str:
         if not var_name in os.environ:
-            raise ValueError(f'{symbol} not found in environment')
+            raise ValueError(f'{var_name} not found in environment')
 
         return os.environ[var_name]
 
 
-    def resolveSecret(self, symbol, context):
-        return context["resolveSecret"](symbol)
+    def resolveSecret(self, var_name: str, context: dict) -> str:
+        return context["resolveSecret"](var_name)
+
--- firestarter/tests/test_preprocessor.py
+++ firestarter/tests/test_preprocessor.py
@@ -1,16 +1,36 @@
 from firestarter.common.preprocessor import PreProcessor
 import os
+import yaml
+import pytest
 
-def load_yaml(file):
-    with open(file, 'r') as f:
+def load_yaml(file_path: str) -> str:
+    with open(file_path, "r") as f:
         return f.read()
 
-def test_parser():
-    pp = PreProcessor(load_yaml(os.path.join(
-        os.path.dirname(__file__), 'fixtures/preprocess_workflow.yaml'
+def process_file(file_path: str) -> str:
+    pp: PreProcessor = PreProcessor(load_yaml(os.path.join(
+        os.path.dirname(__file__), file_path
     )))
 
-    pp.preprocess({
-        'resolveVar': lambda v: f'VAR_{v}',
-        'resolveSecret': lambda s: f'VAR_{s}',
+    return pp.preprocess({
+        "resolveVar": lambda v: f"VAR_{v}",
+        "resolveSecret": lambda s: f"VAR_{s}",
     })
+
+
+def test_parser() -> None:
+    result: dict = yaml.load(
+        process_file("fixtures/preprocess_workflow.yaml"), Loader=yaml.Loader
+    )
+
+    assert result.get("a", {}).get("value", "") == "VAR_SECRET1"
+    assert result.get("b", {})[0].get("name", "") == os.environ["SHELL"]
+    assert result.get("b", {})[0].get("value", "") == "VAR_VALUE"
+
+def test_error() -> None:
+    with pytest.raises(ValueError):
+        result: dict = yaml.load(
+            process_file("fixtures/preprocess_workflow_error.yaml"),
+            Loader=yaml.Loader,
+        )
+
