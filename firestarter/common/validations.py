abb3ac9f11b7a0a734aaa805a0490e87ac3416be
Juan José Vázquez Gil <juanjose.vazquez@prefapp.es>
2023-05-30 08:10:50 +0200

    Added exception if variable name is not in context

--- firestarter/common/validations.py
+++ firestarter/common/validations.py
@@ -5,6 +5,24 @@ import json
 import yaml
 
 
+def process_var(context, var_name: str) -> str:
+    if var_name not in context.vars.keys():
+        raise ValueError(f"{var_name} could not be found in the VARS context")
+
+    return context.vars[var_name]
+
+def process_secret(context, var_name: str) -> str:
+    if var_name not in context.secrets.keys():
+        raise ValueError(f"{var_name} could not be found in the SECRETS context")
+
+    return context.secrets[var_name]
+
+def process_env(context, var_name: str) -> str:
+    if var_name not in os.environ.keys():
+        raise ValueError(f"{var_name} could not be found in the ENV context")
+
+    return os.environ[var_name]
+
 def helper_get_config_schema(schema_path: str) -> dict:
     CONFIG_SCHEMA = False
 
@@ -19,9 +37,9 @@ def validate_config(config_path: str, schema_path: str, context = None) -> dict:
         if context:
             preprocessor: PreProcessor = PreProcessor(config_file.read())
             config_str: str = preprocessor.preprocess({
-                "vars": lambda v: context.vars[v],
-                "secrets": lambda s: context.secrets[s],
-                "env": lambda e: os.environ[e],
+                "vars": lambda v: process_var(context, v),
+                "secrets": lambda s: process_secrets(context, s),
+                "env": lambda e: process_env(context, e),
             })
         else:
             config_str: str = config_file.read()
