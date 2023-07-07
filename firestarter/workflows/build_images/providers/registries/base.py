ad0c93cdf9fbbc1558d133ee0fc74978a1ffb7c6
Jose Alvarez <jose.alvarez@prefapp.es>
2023-07-07 14:14:01 +0200

    Add login strategy for azure

--- firestarter/workflows/build_images/providers/registries/base.py
+++ firestarter/workflows/build_images/providers/registries/base.py
@@ -1,7 +1,13 @@
 from abc import abstractmethod
 import json
 from os import getenv, path
+from dataclasses import dataclass
+import base64
 
+@dataclass
+class RegistryAuth:
+    username: str
+    token: str
 
 class RegistryProvider():
 
@@ -17,13 +23,42 @@ class RegistryProvider():
         pass
 
     def login_registry(self):
+        self._base_login_registry()
+
+    def _base_login_registry(self):
+        print("Base login")
+
+        auth = self.get_registry_auth()
+        auth_string = f"{auth.username}:{auth.token}"
+        b64_auth = base64.b64encode(auth_string.encode('ascii')).decode('ascii')
+
         docker_cfg_path = f"{getenv('HOME')}/.docker/config.json"
         if not path.exists(docker_cfg_path):
             config = {}
         else:
             with open(docker_cfg_path, 'r') as f:
                 config = json.load(f)
-        config.setdefault("auths", {})[self.registry] = {"auth": self.get_registry_auth()}
+        config.setdefault("auths", {})[self.registry] = {"auth": b64_auth}
+
+        # Write the updated config to config.json
+        with open(docker_cfg_path, "w") as f:
+            json.dump(config, f, indent=2)
+    
+    def _oauth_login_registry(self):
+        print("Oauth login")
+        auth = self.get_registry_auth()
+        auth_string = f"{auth.username}:"
+        b64_auth = base64.b64encode(auth_string.encode('ascii')).decode('ascii')
+        docker_cfg_path = f"{getenv('HOME')}/.docker/config.json"
+        if not path.exists(docker_cfg_path):
+            config = {}
+        else:
+            with open(docker_cfg_path, 'r') as f:
+                config = json.load(f)
+        config.setdefault("auths", {})[self.registry] = {
+            "auth": b64_auth,
+            "identitytoken": auth.token
+        }
 
         # Write the updated config to config.json
         with open(docker_cfg_path, "w") as f:
