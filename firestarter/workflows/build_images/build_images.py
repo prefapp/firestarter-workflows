ab9e1b65890506401df40a26883c940265a7286a
frmadem <frmadem@hotmail.com>
2024-04-10 17:57:41 +0200

    Debug

--- firestarter/workflows/build_images/build_images.py
+++ firestarter/workflows/build_images/build_images.py
@@ -5,5 +5,24 @@ logger = logging.getLogger(__name__)
 
 
 class BuildImages(FirestarterWorkflow):
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+        self._secrets = self.resolve_secrets(self.secrets)
+        self._repo_name = self.vars['repo_name']
+        self._snapshots_registry = self.vars['snapshots_registry']
+        self._releases_registry = self.vars['releases_registry']
+        self._auth_strategy = self.vars['auth_strategy']
+        self._type = self.vars['type']
+        self._from = self.vars['from']
+        self._flavors = self.vars['flavors'] if 'flavors' in self.vars else 'default'
+        self._container_structure_filename = self.vars['container_structure_filename'] if 'container_structure_filename' in self.vars else None
+        self._dagger_secrets = []
+        self._login_required = literal_eval(
+            self.vars['login_required'].capitalize()) if 'login_required' in self.vars else True
+        self._publish = self.vars['publish'] if 'publish' in self.vars else True
+        self._already_logged_in_providers = []
+
+        # Read the on-premises configuration file
+        self._config = Config.from_yaml(self.config_file, self.type)
     def execute(self):
-        print(f"Eiqui")
+        print(self)
