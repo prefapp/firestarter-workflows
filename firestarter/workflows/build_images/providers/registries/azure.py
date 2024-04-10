7dd23f2ddcba26484f795a2c33a7f195617af4dc
Toni R. Vega <91343444+tonirvega@users.noreply.github.com>
2024-04-10 16:49:58 +0200

    Update azure.py

--- firestarter/workflows/build_images/providers/registries/azure.py
+++ firestarter/workflows/build_images/providers/registries/azure.py
@@ -11,13 +11,13 @@ class AzureOidcDockerRegistryAuth(RegistryProvider):
         aad_access_token = credential.get_token("https://management.azure.com/.default").token
 
         # Get the AAD refresh token
+        
         data = {
             "grant_type": "access_token",
             "service": self.registry,
             "tenant": "common",
             "access_token": aad_access_token
         }
-
         acr_refresh_token = requests.post(
             f"https://{self.registry}/oauth2/exchange",
             data=data
@@ -26,20 +26,21 @@ class AzureOidcDockerRegistryAuth(RegistryProvider):
         # We should be able to use the access token. However, it doesn't work. So we use the refresh token instead.
 
         # Get the ACR access token
-        data = {
-            "grant_type": "refresh_token",
-            "service": self.registry,
-            "scope": "registry:catalog:*",
-            "refresh_token": acr_refresh_token
-        }
+        # data = {
+        #     "grant_type": "refresh_token",
+        #     "service": self.registry,
+        #     "scope": "registry:catalog:*",
+        #     "refresh_token": acr_refresh_token
+        # }
 
-        acr_access_token = requests.post(
-            f"https://{self.registry}/oauth2/token",
-            data=data
-        ).json()['access_token']
+        # acr_access_token = requests.post(
+        #     f"https://{self.registry}/oauth2/token",
+        #     data=data
+        # ).json()['access_token']
+
 
         # Generate the base64-encoded auth string
-        auth = RegistryAuth(username="00000000-0000-0000-0000-000000000000", token=acr_access_token)
+        auth = RegistryAuth(username="00000000-0000-0000-0000-000000000000", token=acr_refresh_token)
         return auth
 
 
