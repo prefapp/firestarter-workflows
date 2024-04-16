57917721de88d0b70711fe58b9e32bf7a0c1255b
Toni R. Vega <91343444+tonirvega@users.noreply.github.com>
2024-04-16 16:01:36 +0200

    fix: secrets (#49)
    * fix: secrets
    
    * change ref to test new changes
    
    * debug secrets
    
    * debug secrets
    
    * debug secrets
    
    * fix secrets
    
    * fix
    
    * Update build_images.yaml
    
    * fix: remove unused container instance
    
    * change ref to test changes
    
    * Update build_images.yaml
    
    ---------
    
    Co-authored-by: tonirvega <antonio.rodriguez@prefapp.es>

--- firestarter/workflows/build_images/build_images.py
+++ firestarter/workflows/build_images/build_images.py
@@ -146,14 +146,9 @@ class BuildImages(FirestarterWorkflow):
 
     # Define a coroutine function to compile an image using Docker
 
-    async def compile_image_and_publish(self, ctx, build_args, custom_secrets, dockerfile, image):
+    async def compile_image_and_publish(self, ctx, build_args, secrets, dockerfile, image):
         # Set a current working directory
         src = ctx.host().directory(".")
-
-        logger.info(f"Generic secrets: {self.dagger_secrets}")
-        logger.info(f"Custom secrets: {custom_secrets}")
-
-        secrets = self.dagger_secrets + custom_secrets
 
         logger.info(f"Using secrets: {secrets}")
 
@@ -178,63 +173,88 @@ class BuildImages(FirestarterWorkflow):
 
         # Connect to Dagger
         async with dagger.Connection(config) as client:
-            client.container()
 
+            secrets_for_all_flavors = []
             for key, value in self.secrets.items():
-                self._dagger_secrets.append(client.set_secret(key, value))
+                secrets_for_all_flavors.append(client.set_secret(key, value))
 
-            # Set up a task group to execute the compilation process for all flavors in parallel
-            async with anyio.create_task_group() as tg:
-                for flavor in self.flavors:
-                    value = self.config.images[flavor]
-                    # Get the registry, repository, build arguments, Dockerfile path, and address for the current on-premises
-                    registry = self.vars[f"{self.type}_registry"]
-                    build_args = value.build_args or {}
-                    dockerfile = value.dockerfile or ""
-                    extra_registries = value.extra_registries or []
+            for flavor in self.flavors:
 
-                    # Set the build arguments for the current on-premises
-                    build_args_list = [dagger.BuildArg(name=key, value=value) for key, value in build_args.items()]
+                registry, build_args, dockerfile, extra_registries = self.get_flavor_data(flavor)
 
-                    # Print the current on-premises data
-                    print(f'\nOn-premise: {flavor.upper()}')
-                    print(f'\tRegistry: {registry}')
-                    print(f'\tRepository: {self.repo_name}')
-                    if build_args != {}:
-                        print(f'\tBuild args: {build_args}')
-                    print(f'\tDockerfile: {dockerfile}')
+                # Set the build arguments for the current on-premises
+                build_args_list = [dagger.BuildArg(name=key, value=value) for key, value in build_args.items()]
 
-                    custom_secrets = self.config.images[flavor].secrets or {}
-                    custom_secrets = self.resolve_secrets(custom_secrets)
+                resolved_secret_refs = self.resolve_secrets(
+                    
+                    self.config.images[flavor].secrets or {}
+                    
+                )
 
-                    logger.info(f"Setting flavor {flavor} custom secrets: {custom_secrets.keys()}")
+                logger.info(f"Setting flavor {flavor} custom secrets: {resolved_secret_refs.keys()}")
 
-                    custom_dagger_secrets = []
+                # Create an empty list to store the Dagger secrets for the custom secrets for this flavor
+                flavor_secrets = []
 
-                    for key, value in custom_secrets.items():
-                        custom_dagger_secrets.append(
-                            client.set_secret(key, value))
-
-                    # Set the address for the default registry
-                    default_address = f"{registry}/{self.repo_name}"
-                    default_image = f"{default_address}:{normalize_image_tag(self.from_version + '_' + flavor)}"
-                    print(f'\tDefault image name: {default_image}')
-
-                    registry_list = [default_image]
-
-                    for extra_registry in extra_registries:
-                        new_address = f"{extra_registry['name']}/{extra_registry['repository']}"
-                        new_image = f"{new_address}:{normalize_image_tag(self.from_version + '_' + flavor)}"
-
-                        registry_list.append(new_image)
-
-
-                    for image in registry_list:
-                        await tg.spawn(
-                            self.compile_image_and_publish, client,
-                            build_args_list, custom_dagger_secrets,
-                            dockerfile, image
+                for key, value in resolved_secret_refs.items():
+                    
+                    flavor_secrets.append(
+                        
+                        client.set_secret(
+                        
+                            key, 
+                        
+                            value
+                        
                         )
+                    
+                    )
+
+                # Combine generic and custom secrets for this flavor
+                secrets = secrets_for_all_flavors + flavor_secrets
+
+                # Set the address for the default registry
+                registry_adress = f"{registry}/{self.repo_name}"
+                full_registry_adress = f"{registry_adress}:{normalize_image_tag(self.from_version + '_' + flavor)}"
+
+                # Create a list of addresses for all registries
+                registry_list = [full_registry_adress]
+
+                for extra_registry in extra_registries:
+                    
+                    extra_registry_adress = f"{extra_registry['name']}/{extra_registry['repository']}"
+                    
+                    extra_full_registry_adress = f"{extra_registry_adress}:{normalize_image_tag(self.from_version + '_' + flavor)}"
+
+                    registry_list.append(extra_full_registry_adress)
+
+                for image in registry_list:
+
+                    await self.compile_image_and_publish(
+                        client, 
+
+                        build_args_list, 
+                        
+                        secrets,
+                        
+                        dockerfile, 
+                        
+                        image
+                    )
+
+    def get_flavor_data(self, flavor):
+        
+        value = self.config.images[flavor]
+                
+        registry = self.vars[f"{self.type}_registry"]
+        
+        build_args = value.build_args or {}
+        
+        dockerfile = value.dockerfile or ""
+        
+        extra_registries = value.extra_registries or []
+        
+        return registry, build_args, dockerfile, extra_registries
 
 
     def execute(self):
