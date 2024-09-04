d6c684b5028af365a012fa6d38da4422568badf2
Juan José Vázquez Gil <juanjose.vazquez@prefapp.es>
2024-09-04 11:54:26 +0200

    fix: Reverted config related changes

--- firestarter/workflows/build_images/build_images.py
+++ firestarter/workflows/build_images/build_images.py
@@ -52,10 +52,6 @@ class BuildImages(FirestarterWorkflow):
             self.vars['login_required'].capitalize()) if 'login_required' in self.vars else True
         self._publish = self.vars['publish'] if 'publish' in self.vars else True
 
-        # We checkout the correct sha/tag before getting the config to ensure
-        # it's the version that we want
-        self.checkout_git_repository(self._from)
-
         # Read the on-premises configuration file
         self._config = Config.from_yaml(
             self.config_file, self.type, self.secrets)
@@ -141,10 +137,6 @@ class BuildImages(FirestarterWorkflow):
     def output_results(self):
         return self._output_results
 
-    def checkout_git_repository(self, checkout_value):
-        # We checkout self._from before building the image
-        subprocess.run(["git", "checkout", checkout_value])
-
     def dereference_from_input(self, input_value):
         # git tag -l <pattern> checks to see if any tag matches the given pattern.
         # Since we want a tag named exactly as input_value, we input it as a pattern
@@ -223,6 +215,9 @@ class BuildImages(FirestarterWorkflow):
     # Define a coroutine function to compile an image using Docker
 
     async def compile_image_and_publish(self, ctx, build_args, secrets, dockerfile, image):
+        # We checkout self._from before building the image
+        subprocess.run(["git", "checkout", self.from_version])
+
         # Set a current working directory
         src = ctx.host().directory(".")
 
