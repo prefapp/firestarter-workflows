34236722423f3ac13065be7c2b189e6ac629d59c
Juan José Vázquez Gil <juanjose.vazquez@prefapp.es>
2024-09-05 15:23:08 +0200

    test: Test logs

--- firestarter/workflows/build_images/build_images.py
+++ firestarter/workflows/build_images/build_images.py
@@ -171,7 +171,7 @@ class BuildImages(FirestarterWorkflow):
         flavor_filter_list = []
         final_flavors_list = []
 
-        logger.info(f'{all_flavors_list}')
+        logger.info(f'All flavor list: {all_flavors_list}')
 
         # Get the on-premises name from the command-line arguments and filter the on-premises data accordingly
         if self.flavors.replace(' ', '') == '*':
