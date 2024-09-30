9bc80b841ae659079959f4038a1e4a6f377fc210
Javier Gomez Rodriguez <javiergr@ext.zara.com>
2024-09-30 12:04:55 +0200

    feat: add test

---
+++ firestarter/tests/test_build_images_json_schema.py
@@ -0,0 +1,22 @@
+from firestarter.common.validations import validate_config
+from firestarter.workflows.ci.task_manager import TaskManager
+from jsonschema.exceptions import ValidationError
+import pytest
+import os
+
+VALID_CONFIG_FILE_PATH: str =\
+    os.path.join(os.path.dirname(__file__), 'fixtures/flavor.yml')
+
+INVALID_CONFIG_FILE_PATH: str =\
+    os.path.join(os.path.dirname(__file__), 'fixtures/flavor_error.yml')
+
+SCHEMA_FILE_PATH: str = '../workflows/build_images/resources/flavor_schema.json'
+
+def test_validate_schema() -> None:
+    validate_config(VALID_CONFIG_FILE_PATH, SCHEMA_FILE_PATH)
+
+def test_validate_schema_error() -> None:
+    with pytest.raises(ValidationError, match="Additional properties are not allowed"):
+        validate_config(INVALID_CONFIG_FILE_PATH, SCHEMA_FILE_PATH)
+
+
