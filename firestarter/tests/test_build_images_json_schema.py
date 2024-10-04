76a5871da4c68c8f93ce6ea07ebe302089b6e443
Javier Gomez Rodriguez <javiergr@ext.zara.com>
2024-10-04 15:01:31 +0200

    fix: tests

--- firestarter/tests/test_build_images_json_schema.py
+++ firestarter/tests/test_build_images_json_schema.py
@@ -2,6 +2,8 @@ from firestarter.common.validations import validate_config
 from firestarter.workflows.ci.task_manager import TaskManager
 from jsonschema.exceptions import ValidationError, SchemaError
 import pytest
+from unittest import TestCase
+import yaml
 import os
 
 VALID_CONFIG_FILE_PATH: str =\
@@ -16,22 +18,17 @@ INVALID_SCHEMA_FILE_PATH: str =\
     os.path.join(os.path.dirname(__file__), 'fixtures/invalid_jsonschema.json')
 
 def test_validate_schema() -> None:
-    try:
-        validate_config(VALID_CONFIG_FILE_PATH, SCHEMA_FILE_PATH)
-    except FileNotFoundError as fnf_error:
-        pytest.fail(f"File not found: {fnf_error.filename}")
-
+    with open(VALID_CONFIG_FILE_PATH, 'r') as config_file:
+        config_data: dict = yaml.safe_load(config_file)
+        TestCase().assertDictEqual(validate_config(VALID_CONFIG_FILE_PATH, SCHEMA_FILE_PATH), config_data)
 
 def test_validate_schema_error() -> None:
     with pytest.raises(ValidationError, match="Additional properties are not allowed"):
-        try:
-            validate_config(INVALID_CONFIG_FILE_PATH, SCHEMA_FILE_PATH)
-        except FileNotFoundError as fnf_error:
-            pytest.fail(f"File not found: {fnf_error.filename}")
-
-def test_validation_error() -> None:
-    with pytest.raises(ValidationError):
         validate_config(INVALID_CONFIG_FILE_PATH, SCHEMA_FILE_PATH)
+
+def test_validation_file_not_found() -> None:
+    with pytest.raises(FileNotFoundError):
+        validate_config("/tmp/fake_build_images.yaml", SCHEMA_FILE_PATH)
 
 def test_schema_error() -> None:
     with pytest.raises(SchemaError):
