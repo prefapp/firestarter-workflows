05dd3c43ff251599778aee79c8a9023b9fd03eea
Julián <167460039+JulianGV21@users.noreply.github.com>
2024-10-04 14:04:35 +0200

    Update test_build_images_json_schema.py

--- firestarter/tests/test_build_images_json_schema.py
+++ firestarter/tests/test_build_images_json_schema.py
@@ -15,9 +15,6 @@ SCHEMA_FILE_PATH: str = '../workflows/build_images/resources/schema.json'
 INVALID_SCHEMA_FILE_PATH: str =\
     os.path.join(os.path.dirname(__file__), 'fixtures/invalid_jsonschema.json')
 
-def test_validate_schema() -> None:
-    validate_config(VALID_CONFIG_FILE_PATH, SCHEMA_FILE_PATH)
-
 def test_validate_schema_error() -> None:
     with pytest.raises(ValidationError, match="Additional properties are not allowed"):
         validate_config(INVALID_CONFIG_FILE_PATH, SCHEMA_FILE_PATH)
