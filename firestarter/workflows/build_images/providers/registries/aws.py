d509b7849bb06d03ae75ff94ad06e96de05bfdf2
Juan José Vázquez Gil <juanjose.vazquez@prefapp.es>
2026-01-20 13:09:44 +0100

    fix: Set AWS region (test)

--- firestarter/workflows/build_images/providers/registries/aws.py
+++ firestarter/workflows/build_images/providers/registries/aws.py
@@ -5,12 +5,12 @@ import base64
 class AwsOidcDockerRegistryAuth(RegistryProvider):
 
     def get_registry_auth(self):
-        session = boto3.session.Session()
+        session = boto3.session.Session(region_name='eu-west-1')
         ecr = session.client('ecr')
         auth = ecr.get_authorization_token()
         authorization_token = base64.b64decode(
             auth['authorizationData'][0]['authorizationToken'].encode()
         )
-        
+
         username, token = authorization_token.decode().split(':')
         return RegistryAuth(username=username, token=token)
