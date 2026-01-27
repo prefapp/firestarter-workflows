b0459929a19dbda8038c013a747486d2f7493f34
Juan José Vázquez Gil <juanjose.vazquez@prefapp.es>
2026-01-27 14:01:04 +0100

    fix: Return error command

--- firestarter/workflows/build_images/__init__.py
+++ firestarter/workflows/build_images/__init__.py
@@ -1,7 +1,11 @@
 from .build_images import BuildImages
 
 def run(*, vars: dict, secrets: dict, config_file:str):
-    wf = BuildImages(vars=vars, secrets=secrets, config_file=config_file)
-    return wf.execute()
+    try:
+        wf = BuildImages(vars=vars, secrets=secrets, config_file=config_file)
+        return wf.execute()
+    catch Exception as e:
+        logger.error(f"::error title=Dagger Pipeline Failed::{e}")
+        sys.exit(1)
 
 __all__ = [run]
