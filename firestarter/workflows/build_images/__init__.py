d661f269fc81ca20d1c705668822235f234c6f64
Juan José Vázquez Gil <juanjose.vazquez@prefapp.es>
2026-01-27 14:15:10 +0100

    fix: Print

--- firestarter/workflows/build_images/__init__.py
+++ firestarter/workflows/build_images/__init__.py
@@ -5,7 +5,7 @@ def run(*, vars: dict, secrets: dict, config_file:str):
         wf = BuildImages(vars=vars, secrets=secrets, config_file=config_file)
         return wf.execute()
     except Exception as e:
-        logger.error(f"::error title=Dagger Pipeline Failed::{e}")
+        print(f"::error title=Dagger Pipeline Failed::{e}")
         sys.exit(1)
 
 __all__ = [run]
