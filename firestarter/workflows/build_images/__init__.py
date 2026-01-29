9e9031ea873afb1f3f65182927efa7f78127f7f9
juanjosevazquezgil <123170708+juanjosevazquezgil@users.noreply.github.com>
2026-01-29 11:10:02 +0100

    Update firestarter/workflows/build_images/__init__.py
    Co-authored-by: Javier Gómez Rodríguez <javier.gomez@prefapp.es>

--- firestarter/workflows/build_images/__init__.py
+++ firestarter/workflows/build_images/__init__.py
@@ -7,7 +7,7 @@ def run(*, vars: dict, secrets: dict, config_file:str):
         wf = BuildImages(vars=vars, secrets=secrets, config_file=config_file)
         return wf.execute()
     except Exception as e:
-        logging.exception("Fatal error encountered during Dagger pipeline execution.")
+        logging.exception("Fatal error encountered during BuildImages execution.")
         print(f"::error title=BuildImages Failure::{e}")
         sys.exit(1)
 
