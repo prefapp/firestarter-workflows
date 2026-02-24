c433a65d67a51b3d161ac92a54a8eb4945f5be38
Juan José Vázquez Gil <juanjose.vazquez@prefapp.es>
2026-02-24 10:48:25 +0100

    fix: Platform filter test

--- firestarter/tests/test_build_images_functionality.py
+++ firestarter/tests/test_build_images_functionality.py
@@ -442,7 +442,7 @@ async def test_compile_images_filtering_by_platform(mocker) -> None:
 
     result = await builder.compile_images_for_all_flavors()
 
-    assert len(result) == 4
+    assert len(result) == 8
     assert result[0]["flavor"] == "flavor1"
     assert result[0]["repository"] == "xxx/yyy"
     assert result[0]["image_tag"] == "aaaaaaa_flavor1"
@@ -455,6 +455,18 @@ async def test_compile_images_filtering_by_platform(mocker) -> None:
     assert result[3]["flavor"] == "flavor1"
     assert result[3]["repository"] == "repo1"
     assert result[3]["image_tag"] == "flavor1-custom-tag"
+    assert result[4]["flavor"] == "flavor3"
+    assert result[4]["repository"] == "repository3"
+    assert result[4]["image_tag"] == "aaaaaaa_flavor3"
+    assert result[5]["flavor"] == "flavor3"
+    assert result[5]["repository"] == "repository3"
+    assert result[5]["image_tag"] == "flavor3-custom-tag"
+    assert result[6]["flavor"] == "flavor3"
+    assert result[6]["repository"] == "repo3"
+    assert result[6]["image_tag"] == "aaaaaaa_flavor3"
+    assert result[7]["flavor"] == "flavor3"
+    assert result[7]["repository"] == "repo3"
+    assert result[7]["image_tag"] == "flavor3-custom-tag"
 
     builder._platforms = "arm64"
 
