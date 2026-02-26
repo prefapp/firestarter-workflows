8ef5aca7bfd2e261b81c00497ceaa3c9dcbfbbab
Juan José Vázquez Gil <juanjose.vazquez@prefapp.es>
2026-02-26 12:16:02 +0100

    fix: Add ::warning:: command to warning log

--- firestarter/workflows/build_images/build_images.py
+++ firestarter/workflows/build_images/build_images.py
@@ -478,7 +478,7 @@ class BuildImages(FirestarterWorkflow):
 
         if len(results_list) == 0:
             logger.warning(
-                f"No images were built. "
+                f"::warning::No images were built. "
                 f"Please check the workflow filters are correct."
             )
 
