b6b47b79879080e3e1b926e387d7264736b2afa6
Juan José Vázquez Gil <juanjose.vazquez@prefapp.es>
2026-04-28 11:07:30 +0200

    fix: Add missing param

--- firestarter/workflows/build_images/__init__.py
+++ firestarter/workflows/build_images/__init__.py
@@ -4,9 +4,14 @@ from .build_images import BuildImages
 
 logger = logging.getLogger(__name__)
 
-def run(*, vars: dict, secrets: dict, config_file:str):
+def run(*, vars: dict, secrets: dict, additional_build_args: dict, config_file:str):
     try:
-        wf = BuildImages(vars=vars, secrets=secrets, config_file=config_file)
+        wf = BuildImages(
+            vars=vars,
+            secrets=secrets,
+            additional_build_args=additional_build_args,
+            config_file=config_file
+        )
         return wf.execute()
     except Exception as e:
         logger.exception("Fatal error encountered during BuildImages execution.")
